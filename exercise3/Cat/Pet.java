e7b5ae5b72efc6a53936f8eb88d3b8322cd86142
Jasmine Saligan <110054198+jasminesaligan@users.noreply.github.com>
2023-05-11 10:29:20 +0800

    Update Pet.java

--- exercise3/Cat/Pet.java
+++ exercise3/Cat/Pet.java
@@ -1,12 +1,14 @@
 package Cat;
 public class Pet implements PerPin {
 
+    String name = "I have a cat named Moshi";
+    String age = "He is 1 year and 7 months";
+
+
     @Override
     public void meow() {
-        System.out.println("I have a cat named Moshi");
-        System.out.println("He is a PurPin");
-        System.out.println("He is 1 year and 7 months");
         System.out.println("My cute PerPin is meowing");
 
-    };
+    }
+   
 }
