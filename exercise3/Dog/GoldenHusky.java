68d7653d0166a8fe95c22c896975abdca52783d3
Jasmine Saligan <110054198+jasminesaligan@users.noreply.github.com>
2023-05-11 10:30:38 +0800

    Update GoldenHusky.java

--- exercise3/Dog/GoldenHusky.java
+++ exercise3/Dog/GoldenHusky.java
@@ -1,7 +1,13 @@
 package Dog;
-class Golden_Husky extends Golden_Retriever  implements Siberian_Husky {
+class GoldenHusky extends Golden_Retriever  implements Siberian_Husky {
+
+    String name = "I have a dog named Sky";
+    String Sky = "He is a mix breed of Golden Retriever and Husky";
 
     public void bark() {
-        System.out.println("My pomsky is barking");
+        System.out.println("My GoldenHusky is barking");
     }
+
+       
+    
 }
