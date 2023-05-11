27fd0d1c23127816dc3f5a3b1e9ef8489e752477
Jasmine Saligan <110054198+jasminesaligan@users.noreply.github.com>
2023-05-11 10:30:10 +0800

    Update App.java

--- exercise3/Dog/App.java
+++ exercise3/Dog/App.java
@@ -3,10 +3,12 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // Create object from pet class
-        Golden_Husky myPet = new Golden_Husky();
+        GoldenHusky myPet = new GoldenHusky();
 
+        System.out.println(myPet.name);
+        System.out.println(myPet.Sky);
         myPet.bark();
-        System.out.println(myPet.eyeColor);
-        System.out.println(myPet.height);
+        System.out.println("He has a " + myPet.eyeColor + " eyes");
+        System.out.println("He is " + myPet.height + " tall");
     }
 }
