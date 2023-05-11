23ef4c674d63969969d3a3fdb23b5b062c1ec30d
Jasmine Saligan <110054198+jasminesaligan@users.noreply.github.com>
2023-05-11 10:28:43 +0800

    Update App.java

--- exercise3/Cat/App.java
+++ exercise3/Cat/App.java
@@ -5,6 +5,8 @@ public class App {
         // Create object from pet class
         Pet myPet = new Pet();
 
+        System.out.println(myPet.name);
+        System.out.println(myPet.age);
         myPet.meow();
         System.out.println("My PurPin has a " + myPet.eyeColor + " eyes");
         System.out.println("He is " + myPet.height + " tall");
